cc9ef5a9dc5d640b4d5f872a4a85e127d79ddb4c
melon1891 <melon1891ford@gmail.com>
2020-11-14 09:09:18 +0900

    myFirstCommit

---
+++ js-grammar/circle-calc.js
@@ -0,0 +1,3 @@
+const radical=12;
+const area = radical*radical*3.14;
+document.write(area);
---
+++ js-grammar/js-movieticket.js
@@ -0,0 +1,11 @@
+const age;
+const isMember;
+let ticket_price;
+
+if(age<=15){
+    ticket_price = 800;
+}else if(isMember){
+    ticket_price = 1000;
+}else{
+    ticket_price = 1800;
+}
